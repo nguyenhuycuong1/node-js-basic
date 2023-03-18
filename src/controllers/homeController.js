1d926d010fbab6c1afef6683c54d30585e1dd7ae
Nguyen Huy Cuong <nhclnhc@gmail.com>
2023-03-18 16:12:20 +0700

    connect database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,18 @@
+import connection from '../configs/connectDb';
+
+
 
 const getHomePage = (req, res) => {
-    res.render('index.ejs');
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users` ',
+        function(err, results, fields) {
+            console.log('>>>mysql');
+            console.log(results); // results contains rows returned by server
+            data = results.map(row => row);
+            return res.render('index.ejs', { dataUser: JSON.stringify(data) });
+        }
+    );
 }
 
 module.exports = {
