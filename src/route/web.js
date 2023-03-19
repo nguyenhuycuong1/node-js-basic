d4b3f67f475e338e2d3a2cf450d4107fb28cec49
Nguyen Huy Cuong <nhclnhc@gmail.com>
2023-03-19 18:02:15 +0700

    upload single file

--- src/route/web.js
+++ src/route/web.js
@@ -1,8 +1,35 @@
 import express from 'express';
+import multer from 'multer';
+import path from 'path';
+
+const appRoot = require('app-root-path');
 
 import homeController from '../controllers/homeController';
 
 let router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, appRoot + "/src/public/images/");
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+const upload = multer({ storage: storage, fileFilter: imageFilter });
+
 
 const initWebRoute = (app) => {
 
@@ -18,6 +45,8 @@ const initWebRoute = (app) => {
 
     router.post('/update-user', homeController.updateUser)
 
+    router.get('/upload', homeController.getUploadFilePage);
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
     return app.use('/', router);
 }
 
