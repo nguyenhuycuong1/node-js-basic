87d80727c2942bd02abf88e62028021ba18b822f
Nguyen Huy Cuong <nhclnhc@gmail.com>
2023-03-21 18:39:58 +0700

    back to upload single file

--- src/route/web.js
+++ src/route/web.js
@@ -15,8 +15,7 @@ const initWebRoute = (app) => {
     router.post('/update-user', homeController.updateUser)
 
     router.get('/upload', homeController.getUploadFilePage);
-    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile);
-    router.post('/upload-multiple-images', upload.array('multiple_images', 10), homeController.handleUploadMultipleFile);
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
     return app.use('/', router);
 }
 
